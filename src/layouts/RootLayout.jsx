e8772aacf931edf0468606d17acf66500d292297
Rownok Zahan <rownokzahan9@gmail.com>
2024-09-29 16:36:41 +0600

    Implement scroll-to-top functionality in RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const RootLayout = () => {
+  const { pathname } = useLocation();
+
+  // Scroll to the top of the page whenever the pathname changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="text-dark-deep text-base bg-background-light">
       <Outlet />
