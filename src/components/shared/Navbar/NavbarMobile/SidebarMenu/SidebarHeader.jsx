545df6b295aa1d5721b07b34d7c474f4b69bb249
Rownok Zahan <rownokzahan9@gmail.com>
2024-09-29 11:01:31 +0600

    Add Overlay Effect On the Sidebar

--- src/components/shared/Navbar/NavbarMobile/SidebarMenu/SidebarHeader.jsx
+++ src/components/shared/Navbar/NavbarMobile/SidebarMenu/SidebarHeader.jsx
@@ -2,7 +2,7 @@ import { IoClose } from "react-icons/io5";
 
 const SidebarHeader = ({ toggleMenu }) => {
   return (
-    <div className="text-end px-4 py-2 border-b sticky top-0 bg-background-light">
+    <div className="text-end px-4 py-2 border-b sticky top-0 z-10 bg-background-light">
       <button className="text-2xl" onClick={toggleMenu}>
         <IoClose />
       </button>
--- src/components/shared/Navbar/NavbarMobile/SidebarMenu/index.jsx
+++ src/components/shared/Navbar/NavbarMobile/SidebarMenu/index.jsx
@@ -1,20 +1,55 @@
 import SidebarHeader from "./SidebarHeader";
 import CategoryList from "./CategoryList";
 import SidebarFooter from "./SidebarFooter";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const SidebarMenu = ({ isOpen, toggleMenu }) => {
+  const location = useLocation();
+
+  // Handle disabling main page scrolling when the sidebar is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden"; // Disable main page scrolling
+    } else {
+      document.body.style.overflow = ""; // Re-enable scrolling when sidebar closes
+    }
+
+    // Cleanup on component unmount or when closing
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  // Close the sidebar on route change
+  useEffect(() => {
+    if (isOpen) {
+      toggleMenu();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <div
-      className={`fixed top-0 left-0 overflow-scroll w-64 bg-background-light shadow-lg transition-transform transform ${
-        isOpen ? "translate-x-0" : "-translate-x-full"
-      } h-full z-50`}
-    >
-      <SidebarHeader toggleMenu={toggleMenu} />
+    <>
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-[#f9eddf] bg-opacity-50 z-50"
+          onClick={toggleMenu}
+        ></div>
+      )}
 
-      <CategoryList />
-
-      <SidebarFooter />
-    </div>
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 overflow-y-auto w-64 bg-background-light shadow-lg transition-transform transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } h-full z-50`}
+      >
+        <SidebarHeader toggleMenu={toggleMenu} />
+        <CategoryList />
+        <SidebarFooter />
+      </div>
+    </>
   );
 };
 
