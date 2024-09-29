a8a12156e10095116466ecd22f55e68bf258502f
Rownok Zahan <rownokzahan9@gmail.com>
2024-09-29 16:28:56 +0600

    Fix Input animation issue

--- src/components/shared/Navbar/NavbarDesktop/SearchBar.jsx
+++ src/components/shared/Navbar/NavbarDesktop/SearchBar.jsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import { MdOutlineSearch } from "react-icons/md";
 
 const SearchBar = () => {
-  const [showInput, setShowInput] = useState(false); // State to manage input visibility
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState(""); // Manage input value with state
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
   const toggleInput = () => {
-    setShowInput(!showInput); // Toggle the input visibility
+    if (inputValue === "") {
+      setShowInput(!showInput);
+    }
   };
 
   return (
@@ -18,33 +25,27 @@ const SearchBar = () => {
         <input
           type="text"
           id="desktop-search"
+          value={inputValue}
+          onChange={handleInputChange}
           autoComplete="off"
-          placeholder=" "
+          placeholder=""
           className="bg-transparent focus:outline-none peer w-24"
         />
 
         <label
           htmlFor="desktop-search"
-          className="
-            absolute 
-            left-0 
-            transition-all 
-            duration-300
-            peer-focus:text-transparent
-            peer-focus:after:scale-x-100
-            after:absolute 
-            after:left-0 
-            after:bottom-0 
-            after:w-24 
-            after:h-[2px] 
-            after:bg-black 
-            after:scale-x-0 
-            after:transition-transform 
-            after:duration-300
-          "
+          className={`absolute left-0 transition-all duration-300 ${
+            inputValue ? "text-transparent" : ""
+          }`}
         >
           Search Here...
         </label>
+
+        <span
+          className={`w-24 h-px absolute left-0 bottom-0 bg-dark-deep ${
+            inputValue ? "scale-x-100" : "scale-x-0"
+          } peer-focus:scale-x-100 transition-transform duration-300`}
+        ></span>
       </div>
 
       <button onClick={toggleInput}>
