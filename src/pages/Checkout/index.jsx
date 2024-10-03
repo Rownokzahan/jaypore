851f9eec7cb50dd1995562b8c96f47337e3cbcc2
Rownok Zahan <rownokzahan9@gmail.com>
2024-10-03 12:10:23 +0600

    Add payment method section on checkout page

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import DeliveryInfoForm from "../../components/form/DeliveryInfoForm";
 import { Link } from "react-router-dom";
+import { FaCheckCircle } from "react-icons/fa";
 
 const Checkout = () => {
   const {
@@ -30,41 +31,52 @@ const Checkout = () => {
             <DeliveryInfoForm register={register} errors={errors} />
           </div>
 
-          <div className="space-y-4 p-4 md:p-6 bg-background-light">
-            <div className="flex items-center justify-between">
-              <h5 className="text-lg font-semibold">Order Summary</h5>
-              <Link
-                to={"/cart"}
-                className="text-primary underline underline-offset-1"
+          <div className="flex flex-col gap-3 md:gap-6">
+            <div className="p-4 md:p-6 bg-background-light">
+              <h5 className="text-lg font-semibold mb-3">Payment Method</h5>
+
+              <div className="w-full px-4 py-2 border border-gray-300 rounded text-dark relative">
+                <span>Cash On Delivery</span>
+                <FaCheckCircle className="absolute right-3 top-1/2 -translate-y-1/2" />
+              </div>
+            </div>
+
+            <div className="space-y-4 p-4 md:p-6 bg-background-light">
+              <div className="flex items-center justify-between">
+                <h5 className="text-lg font-semibold">Order Summary</h5>
+                <Link
+                  to={"/cart"}
+                  className="text-primary underline underline-offset-1"
+                >
+                  Edit Cart
+                </Link>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <h5 className="text-dark">Order Value</h5>
+                <p className="font-semibold">₹ {1400}</p>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <h5 className="text-dark">Shipping Charges</h5>
+                <p className="uppercase font-semibold">Free</p>
+              </div>
+
+              <hr />
+
+              <div className="flex items-center justify-between !mb-5">
+                <h5 className="text-lg font-semibold text-dark">Grand Total</h5>
+                <p className="font-semibold text-lg">₹ {"560,000"}</p>
+              </div>
+
+              <button
+                onClick={handleSubmit(onSubmit)}
+                className="btn-primary uppercase"
+                disabled={!isValid}
               >
-                Edit Cart
-              </Link>
+                Place Order
+              </button>
             </div>
-
-            <div className="flex items-center justify-between">
-              <h5 className="text-dark">Order Value</h5>
-              <p className="font-semibold">₹ {1400}</p>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <h5 className="text-dark">Shipping Charges</h5>
-              <p className="uppercase font-semibold">Free</p>
-            </div>
-
-            <hr />
-
-            <div className="flex items-center justify-between !mb-5">
-              <h5 className="text-lg font-semibold text-dark">Grand Total</h5>
-              <p className="font-semibold text-lg">₹ {"560,000"}</p>
-            </div>
-
-            <button
-              onClick={handleSubmit(onSubmit)}
-              className="btn-primary uppercase"
-              disabled={!isValid}
-            >
-              Place Order
-            </button>
           </div>
         </div>
       </div>
